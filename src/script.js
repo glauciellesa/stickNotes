949c894aa7e3e329f5c50586e072c28faccd0fb2
Glaucielle <glauciellesa@gmail.com>
2022-10-11 09:45:53 +0200

    CSS color stick

--- src/script.js
+++ src/script.js
@@ -2,18 +2,17 @@ const bntSubmit = document.getElementById("bntSubmit")
 
 bntSubmit.addEventListener("click", (e) => {
   e.preventDefault()
+
   const titleInput = document.getElementById("new-note-title-input")
-  const titleValue = titleInput.value
-
   const noteBodyInput = document.getElementById("new-note-body-input")
-  const noteBodyValue = noteBodyInput.value
-
   const ul = document.getElementById("notes")
+
   const li = document.createElement("li")
   const titleLi = document.createElement("h2")
   const bodyLi = document.createElement("p")
-  const textH2Li = document.createTextNode(titleValue)
-  const textbodyLi = document.createTextNode(noteBodyValue)
+
+  const textH2Li = document.createTextNode(titleInput.value)
+  const textbodyLi = document.createTextNode(noteBodyInput.value)
 
   ul.appendChild(li)
   li.appendChild(titleLi)
